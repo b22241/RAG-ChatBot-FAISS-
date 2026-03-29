49e3dbe7283556c88db739df0c435e2abb4128bd
b22241 <freesoul6800@gmail.com>
2026-03-29 16:09:20 +0530

    clean deplo11y

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ else:
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.output_parsers import StrOutputParser
